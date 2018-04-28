aacfe1f03c0452bbe7f9c5362d678a0d83272948
Arishtanemi2 <solankimihir7744@gmail..com>
2018-04-28 12:59:46 +0530

    bot features6

--- OWTBot.py
+++ OWTBot.py
@@ -15,8 +15,11 @@ f.close();
 f = open('dialogues.txt', 'r')
 dialogues = list(f)
 f.close();
+f = open('scrims.txt', 'r')
+scrims = list(f)
+f.close();
 bot = commands.Bot(command_prefix='!', description='')
-
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -32,7 +35,7 @@ async def giveaway(pas: str):
 async def add(a: str):
     users.append(a)
     f = open('participants.txt', 'a')
-    f.write(a+'\n')
+    f.write(a+'\n') 
     f.close();
     await bot.say('added '+ a)
 @bot.command()
@@ -50,21 +53,33 @@ async def info():
     embed.add_field(name="Author:", value="Arishtanemi")
     # command list for general users
     await bot.say(embed=embed)
+@bot.command(pass_context=True)
+async def startscrim(ctx,play:str,time:str):
+    msg = '{0.author.mention} wants to play '.format(ctx.message)
+    save= '{0.author.mention}'.format(ctx.message)
+    f=open('scrims.txt','a+');
+    f.write(save+' '+play+' '+time+'\n')
+    scrims.append(save+' '+play+' '+time)
+    f.close();
+    await bot.say(msg+play+' at '+time)
 @bot.command()
-async def startscrim(self,message,args):
-       await bot.say(message.author)  
+async def showscrims():
+    embed = discord.Embed(title="Scrims Scheduled", description="", color=0xff0000)
+    for i in range(0,scrims.__len__()):
+        embed.add_field(name=str(i+1), value=str(scrims[i]))
+    await bot.say(embed=embed)
 @bot.command()
 async def help():
     embed = discord.Embed(title="My Commands", description="Use the following to interact with me", color=0x0080ff)
     #add user to giveawaY COMMAND
     embed.add_field(name="!add <Your Username>", value="Add your name as a giveaway participant")
     #extra featureslist
-    embed.add_field(name= "!bored",value="Fetches a random GIF")
+    embed.add_field(name= "!bored",value="Bored or server dead? Fetches a random GIF to keep you entertained")
     embed.add_field(name="!dialogue", value="I tell a random Telugu Movie Dialogue")
 
     await bot.say(embed=embed)
     
 @bot.command()
 async def bored():
-       await bot.say("http://imgur.com/random ")
+       await bot.say("http://imgur.com/random")
 bot.run('NDM3NTAzNDE4ODAyNjM0NzUy.Db9F0w.kxrmOB_5zYr3713w_MI3pL6JFGI')
